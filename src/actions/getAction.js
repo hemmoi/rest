5c51fa0a31d3c6ade745b4e0ca1103cd60261d3b
Kimmo Halonen <hemmoi@gmail.com>
2018-06-06 08:04:28 +0300

    middleware added

--- src/actions/getAction.js
+++ src/actions/getAction.js
@@ -1,1 +1,13 @@
+import {CALL_API, HTTP_GET} from '../middleware/api';
+
 export const GET_DATA = 'GET_DATA';
+export const GET_SUCCESS = 'GET_SUCCESS';
+export const GET_FAIL = 'GET_FAIL';
+
+export const getData = () => ({
+    [CALL_API]: {
+        endpoint: `post`,
+        types:[GET_DATA, GET_SUCCESS, GET_FAIL],
+        method: HTTP_GET
+    }
+});
--- src/components/Get.js
+++ src/components/Get.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {getData} from '../actions/getAction';
 
 class Get extends Component {
+    componentWillMount() {
+        console.log(this.props.getData());
+    }
     render() {
         return (
             <div>Get component</div>
@@ -8,4 +13,9 @@ class Get extends Component {
     }
 }
 
-export default Get;
+const mapStateToProps = (state) => {
+    const data = state.getData.data;
+    return {data}
+}
+
+export default connect(mapStateToProps, {getData})(Get);
